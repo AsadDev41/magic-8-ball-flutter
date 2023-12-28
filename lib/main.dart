b333b6540678dd21a7a7e000233e4dbe07928360
Asad <stormyboye5987@gmail.com>
2023-12-28 04:06:07 -0800

    magicball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 101, 171, 228),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Magic  ball Flutter'),
+        ),
+        body: MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int faceball = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  faceball = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$faceball.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
